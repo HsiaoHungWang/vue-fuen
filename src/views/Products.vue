354a7c63eee365ad3451de337078e2a771daddd3
HsiaoHung Wang <wang@ispan.com.tw>
2023-07-29 14:42:59 +0800

    顯示api回傳的資料

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,17 +1,38 @@
 <template>
         <h2>Products Page</h2>
+        <table class="table table-bordered">
+                <thead>
+                        <tr>
+                                <th>產品編號</th>
+                                <th>產品名稱</th>
+                                <th>產品價格</th>
+                                <th>庫存量</th>
+                        </tr>
+                </thead>
+                <tbody>
+                        <tr v-for="product in products" :key="product.id">
+                        <td>{{ product.id }}</td>
+                        <td>{{ product.name }}</td>
+                        <td>{{ product.price}}</td>
+                        <td>{{ product.stock }}</td>
+                        </tr>
+                </tbody>
+        </table>
 </template>
     
 <script setup>
+   import {ref, reactive} from 'vue'
     //ajax
     //XMLHttpRequest、fetch
     //jQuery $.ajax() $.get()....
     //axios
+   const products = ref([])
 
     const loadProducts = async ()=>{
       const response = await fetch('https://localhost:7192/api/Products')
       const datas = await response.json()
-      console.log(datas)
+      products.value = datas
+      console.log(products.value)
     }
     loadProducts()
 </script>
