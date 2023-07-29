3f87b5b3b6b17c5bd1aaa0c4969458cc9ea7e620
HsiaoHung Wang <wang@ispan.com.tw>
2023-07-29 13:35:10 +0800

    router-link

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
 </script>
 
 <template>
+   <router-link to="/">Go to Home</router-link> | 
+   <router-link to="/about">About</router-link> | 
+   <router-link to="/contact">Contact</router-link>
+  <hr />
   <h2>Hello Vue!!</h2>
   <router-view />
 </template>
