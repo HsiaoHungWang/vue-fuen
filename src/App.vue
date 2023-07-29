51361fcdbfe179d65d9e8d2580f67a14ddd05496
HsiaoHung Wang <wang@ispan.com.tw>
2023-07-29 11:51:51 +0800

    新增Vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Contact from './views/Contact.vue';
 </script>
 
 <template>
   <h2>Hello Vue!!</h2>
+  <Home></Home>
+  <About></About>
+  <Contact></Contact>
 </template>
 
 <style scoped>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+        <h2>About Page</h2>
+</template>
+    
+<script setup lang='ts'>
+    
+</script>
+    
+<style>
+    
+</style>
